cdf152abc92dec3dc475ca5b241ad1ef76004f99
Kwon770 <sckwon770@gmail.com>
2021-01-29 22:15:29 +0900

    feat: Impl google login, logout in AuthContext

--- App.js
+++ App.js
@@ -4,37 +4,9 @@ import NavController from "./components/NavController";
 import "react-native-gesture-handler";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  // const googleLogin = async () => {
-  //   const GOOLGE_ID =
-  //     "255552505547-cqontmt71i4itsctd7l6aa39qajbmnq5.apps.googleusercontent.com";
-
-  //   try {
-  //     const result = await Google.logInAsync({
-  //       androidClientId: GOOLGE_ID,
-  //       scopes: ["profile", "email"],
-  //     });
-  //     if (result.type === "success") {
-  //       const user = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-  //         headers: { Authorization: `Bearer ${result.accessToken}` },
-  //       });
-  //       const { email, family_name, given_name } = await user.json();
-  //       console.log(email);
-  //       console.log(given_name);
-  //       console.log(family_name);
-  //     } else {
-  //       return { cancelled: true };
-  //     }
-  //   } catch (e) {
-  //     return { error: true };
-  //   }
-  // };
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    // <View>
-    //   <Button title="123" onPress={googleLogin} />
-    // </View>
     <AuthProvider isLoggedIn={isLoggedIn}>
       <NavController />
     </AuthProvider>
--- components/AuthContext.js
+++ components/AuthContext.js
@@ -1,15 +1,41 @@
 import React from "react";
 import { createContext, useContext, useState } from "react";
+import * as Google from "expo-google-app-auth";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ isLoggedIn: initIsLoggedIn, children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(initIsLoggedIn);
+  const GOOLGE_ID =
+    "255552505547-cqontmt71i4itsctd7l6aa39qajbmnq5.apps.googleusercontent.com";
+
   const logIn = async () => {
-    console.log("a");
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: GOOLGE_ID,
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+        const user = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+          headers: { Authorization: `Bearer ${result.accessToken}` },
+        });
+        const { email, family_name, given_name } = await user.json();
+
+        setIsLoggedIn(true);
+      } else {
+        console.log("[Event] Google login cancel");
+      }
+    } catch (e) {
+      console.log("[Event] Google login error :  " + e);
+    }
   };
 
-  const logOut = async () => {};
+  const logOut = async () => {
+    await Google.logOutAsync({ androidClientId: GOOLGE_ID });
+
+    setIsLoggedIn(false);
+  };
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, logIn, logOut }}>
