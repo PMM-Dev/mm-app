fc12a9a5d0d00b940de8e6f4bbf5bf0becc40ade
sjmjys954646 <sjmjys954646@gmail.com>
2021-01-29 21:27:31 +0900

    fix : authscreen

--- screen/Auth.js
+++ screen/Auth.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
 import { Button } from "react-native-paper";
+import { useLogIn } from "../components/AuthContext";
 
 const View = styled.View`
   justify-content: center;
@@ -22,18 +23,14 @@ const Title = styled.Text`
 `;
 
 const Auth = () => {
+  const logIn = useLogIn();
   return (
     <View>
       <View>
         <Title>로고</Title>
       </View>
       <View>
-        <Button
-          icon="google"
-          mode="contained"
-          color="#DC143C"
-          onPress={() => console.log("Pressed")}
-        >
+        <Button icon="google" mode="contained" color="#DC143C" onPress={logIn}>
           <Bold>구글 계정으로 로그인</Bold>
         </Button>
       </View>
