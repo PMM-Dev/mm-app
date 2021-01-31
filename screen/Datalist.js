abfa49dea659b7d136748ac90ae961dc55d8143a
sjmjys954646 <sjmjys954646@gmail.com>
2021-01-31 20:52:32 +0900

    feat : datalist screen

--- screen/Datalist.js
+++ screen/Datalist.js
@@ -2,89 +2,128 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Searchbar, Button, List, Checkbox } from "react-native-paper";
-
-const HeadView = styled.View`
-  justify-content: center;
-  align-items: stretch;
-  background-color: white;
-  flex: 1;
-`;
-
-const ButtonView = styled.View``;
-
-const AcordianList = styled.View`
-  flex: 3;
-`;
-
-const SearchBar = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-`;
+import { ScrollView } from "react-native";
 
 const Bold = styled.Text`
   font-weight: 500;
   color: white;
 `;
 
+const Screen = styled.ScrollView`
+  margin-top: 60px;
+  margin-bottom: 10px;
+  background-color: white;
+`;
+
 const Auth = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
-  const [expanded, setExpanded] = React.useState(true);
-  const handlePress = () => setExpanded(!expanded);
-  const [checked, setChecked] = React.useState(false);
+  const [checked11, setChecked11] = React.useState(false);
+  const [checked12, setChecked12] = React.useState(false);
+  const [checked13, setChecked13] = React.useState(false);
+  const [checked21, setChecked21] = React.useState(false);
+  const [checked22, setChecked22] = React.useState(false);
+  const [checked23, setChecked23] = React.useState(false);
+  const [checked31, setChecked31] = React.useState(false);
+  const [checked32, setChecked32] = React.useState(false);
+  const [checked33, setChecked33] = React.useState(false);
+
   return (
-    <HeadView>
-      <SearchBar>
-        <Searchbar
-          placeholder="Search"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
+    <Screen>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <List.Section>
+        <List.Accordion
+          title="음식 종류"
+          left={(props) => <List.Icon {...props} icon="food" />}
+        >
+          <Checkbox.Item
+            label="한식"
+            status={checked11 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked11(!checked11);
+            }}
+          />
+          <Checkbox.Item
+            label="중식"
+            status={checked12 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked12(!checked12);
+            }}
+          />
+          <Checkbox.Item
+            label="양식"
+            status={checked13 ? "checked" : "unchecked"}
+            onPress={() => {
+              setChecked13(!checked13);
+            }}
+          />
+        </List.Accordion>
+      </List.Section>
+      <List.Accordion
+        title="가격"
+        left={(props) => <List.Icon {...props} icon="currency-usd" />}
+      >
+        <Checkbox.Item
+          label="0~4000"
+          status={checked21 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked21(!checked21);
+          }}
         />
-      </SearchBar>
-      <AcordianList>
-        <List.Section title="필터링">
-          <List.Accordion
-            title="음식 종류"
-            left={(props) => <List.Icon {...props} icon="folder" />}
-          >
-            <List.Item title="First item" />
-            <List.Item title="Second item" />
-            <Checkbox
-              status={checked ? "checked" : "unchecked"}
-              onPress={() => {
-                setChecked(!checked);
-              }}
-            />
-          </List.Accordion>
+        <Checkbox.Item
+          label="4000~8000"
+          status={checked22 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked22(!checked22);
+          }}
+        />
+        <Checkbox.Item
+          label="8000~"
+          status={checked23 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked23(!checked23);
+          }}
+        />
+      </List.Accordion>
+      <List.Accordion
+        title="위치"
+        left={(props) => <List.Icon {...props} icon="map-marker" />}
+      >
+        <Checkbox.Item
+          label="정문"
+          status={checked31 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked31(!checked31);
+          }}
+        />
+        <Checkbox.Item
+          label="후문"
+          status={checked32 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked32(!checked32);
+          }}
+        />
+        <Checkbox.Item
+          label="쪽문"
+          status={checked33 ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked33(!checked33);
+          }}
+        />
+      </List.Accordion>
 
-          <List.Accordion
-            title="Controlled Accordion"
-            left={(props) => <List.Icon {...props} icon="folder" />}
-            expanded={expanded}
-            onPress={handlePress}
-          >
-            <List.Item title="First item" />
-            <List.Item title="Second item" />
-          </List.Accordion>
-
-          <List.Accordion
-            title="Controlled Accordion"
-            left={(props) => <List.Icon {...props} icon="folder" />}
-            expanded={expanded}
-            onPress={handlePress}
-          >
-            <List.Item title="First item" />
-            <List.Item title="Second item" />
-          </List.Accordion>
-        </List.Section>
-      </AcordianList>
-      <ButtonView>
-        <Button icon="google" mode="contained" color="#DC143C">
-          <Bold>구글 계정으로 로그인</Bold>
-        </Button>
-      </ButtonView>
-    </HeadView>
+      <Button
+        mode="contained"
+        color="#8A2BE2"
+        onPress={() => console.log("Pressed")}
+      >
+        <Bold>검색</Bold>
+      </Button>
+    </Screen>
   );
 };
 
