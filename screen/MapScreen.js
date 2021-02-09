572468ba31aefe2ef348e3b7bd9f9e1020d590d6
sjmjys954646 <sjmjys954646@gmail.com>
2021-02-09 16:23:54 +0900

    feat : load before initialize

--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,233 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Button } from "react-native-paper";
+import { ActivityIndicator, Button, Colors } from "react-native-paper";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
-
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  background-color: white;
-  flex: 1;
-`;
-
-const Container = styled.View`
-  width: 300px;
-  height: 300px;
-`;
-
-const mapStyle = [
-  {
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#212121",
-      },
-    ],
-  },
-  {
-    elementType: "labels.icon",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#757575",
-      },
-    ],
-  },
-  {
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#212121",
-      },
-    ],
-  },
-  {
-    featureType: "administrative",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#757575",
-      },
-    ],
-  },
-  {
-    featureType: "administrative.country",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#9e9e9e",
-      },
-    ],
-  },
-  {
-    featureType: "administrative.land_parcel",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "administrative.locality",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#bdbdbd",
-      },
-    ],
-  },
-  {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#757575",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#181818",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#616161",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#1b1b1b",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#2c2c2c",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#8a8a8a",
-      },
-    ],
-  },
-  {
-    featureType: "road.arterial",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.arterial",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#373737",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#3c3c3c",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway.controlled_access",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#4e4e4e",
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#616161",
-      },
-    ],
-  },
-  {
-    featureType: "transit",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#757575",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#000000",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#3d3d3d",
-      },
-    ],
-  },
-];
 
 const MapScreen = () => {
   const [marker, setmarker] = useState([
@@ -254,48 +30,107 @@ const MapScreen = () => {
       longitude: 126.90583484216211,
     },
   });
+  const [isLoading, setIsLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
+  const preLoad = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
 
-      let curlocation = await Location.getCurrentPositionAsync({});
-      setLocation(curlocation);
-    })();
+    let curlocation = await Location.getCurrentPositionAsync({});
+    setLocation(curlocation);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    preLoad();
   }, []);
+
   if (errorMsg) {
   }
 
   return (
-    <View>
-      <Container>
-        <MapView
-          style={{ flex: 1 }}
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.009,
-            longitudeDelta: 0.009,
-          }}
-          provider={PROVIDER_GOOGLE}
-          customMapStyle={mapStyle}
-        >
-          {marker.map((makrer, index) => (
-            <Marker
-              key={index}
-              coordinate={makrer.latlng}
-              title={marker.title}
-              description={marker.description}
-            />
-          ))}
-        </MapView>
-      </Container>
-    </View>
+    <>
+      {isLoading ? (
+        <View>
+          <ActivityIndicator
+            animating={true}
+            size="large"
+            color={Colors.red800}
+          />
+        </View>
+      ) : (
+        <View>
+          <Container>
+            <MapView
+              style={{ flex: 1 }}
+              initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.009,
+                longitudeDelta: 0.009,
+              }}
+              provider={PROVIDER_GOOGLE}
+              customMapStyle={mapStyle}
+            >
+              {marker.map((makrer, index) => (
+                <Marker
+                  key={index}
+                  coordinate={makrer.latlng}
+                  title={marker.title}
+                  description={marker.description}
+                />
+              ))}
+            </MapView>
+          </Container>
+        </View>
+      )}
+    </>
   );
 };
+
+const View = styled.View`
+  background-color: white;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.View`
+  width: 300px;
+  height: 300px;
+`;
+
+const mapStyle = [
+  {
+    featureType: "road.arterial",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
 
 export default MapScreen;
