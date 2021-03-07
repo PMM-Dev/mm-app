d99641761fe31ba757107acd872a5934a1d97239
Kwon770 <sckwon770@gmail.com>
2021-03-07 14:26:41 +0900

    feat: Change Restaurant Enum

--- Restaurant.js
+++ Restaurant.js
@@ -1,24 +1,24 @@
 const Type = Object.freeze({
-  Korean: 1,
-  Flour: 2,
-  Dessert: 3,
-  Japanese: 4,
-  Fastfood: 5,
-  Western: 6,
-  Asain: 7,
-  Nightfood: 8,
+  Korean: 0,
+  Flour: 1,
+  Dessert: 2,
+  Japanese: 3,
+  Fastfood: 4,
+  Western: 5,
+  Asain: 6,
+  Nightfood: 7,
 });
 
 const Price = Object.freeze({
-  Cheap: 1,
-  Reasonable: 2,
-  Expensive: 3,
+  Cheap: 0,
+  Reasonable: 1,
+  Expensive: 2,
 });
 
 const location = Object.freeze({
-  FrontGate: 1,
-  SideGate: 2,
-  BackGate: 3,
+  FrontGate: 0,
+  SideGate: 1,
+  BackGate: 2,
 });
 
 export default { ...Type, ...Price, ...location };
