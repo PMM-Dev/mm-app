2961545a27991929f985553699db0462113d4ace
sjmjys954646 <sjmjys954646@gmail.com>
2021-05-16 14:05:22 +0900

    feat : conneection with server home-westernList

--- screen/restaurantList/WesternList.js
+++ screen/restaurantList/WesternList.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Button } from "react-native-paper";
+import { API_URL, API_TOKEN } from "@env";
+import axios from "axios";
 
 const Holder = styled.View`
   flex: 1;
@@ -10,13 +12,36 @@ const Holder = styled.View`
 
 const Title = styled.Text``;
 
+const Test = styled.Text`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 const WesternList = ({ navigation: { navigate } }) => {
+  const [data, setData] = useState(undefined);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/v1/restaurant/list`)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+  useEffect(() => {}, [data]);
   return (
     <Holder>
       <Title>Western</Title>
-      <Button mode="contained" onPress={() => navigate("Restaurant")}>
-        List
-      </Button>
+      {data != undefined ? (
+        data.map((element, key) => <Test key={key}>{element.name}</Test>)
+      ) : (
+        <Test />
+      )}
     </Holder>
   );
 };
