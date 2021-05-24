ee7410edc6642fdcb2fbf2d8aef164d583423f3d
sjmjys954646 <sjmjys954646@gmail.com>
2021-05-24 15:13:49 +0900

    feat : HomeDesign fin

---
+++ components/Home/PostNoticeMini.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import { TouchableOpacity } from "react-native";
+
+const PostNoticeMini = ({ description, year, month, day }) => {
+  return (
+    <Card>
+      <TouchableOpacity
+        style={{ flex: 1, flexDirection: "row" }}
+        onPress={() => console.log("pressed")}
+      >
+        <Title>
+          <Title_Text>{description}</Title_Text>
+        </Title>
+        <Date>
+          <Date_Text>
+            {year}-{month >= 10 ? null : 0}
+            {month}-{day}
+          </Date_Text>
+        </Date>
+      </TouchableOpacity>
+    </Card>
+  );
+};
+
+const Card = styled.View`
+  overflow: hidden;
+  width: 100%;
+  flex: 1;
+  flex-direction: row;
+`;
+
+const Title = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+`;
+
+const Title_Text = styled.Text`
+  margin-left: 30px;
+  font-size: 12px;
+  font-weight: 700;
+  color: #000000;
+  text-align: left;
+  width: 100%;
+`;
+
+const Date = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+`;
+
+const Date_Text = styled.Text`
+  width: 100%;
+  font-size: 10px;
+  font-weight: 100;
+  margin-left: 30px;
+`;
+
+export default PostNoticeMini;
--- screen/Home.js
+++ screen/Home.js
@@ -11,10 +11,12 @@ import styled from "styled-components";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import Theme from "../style/Theme";
 import RestaurantMenu from "../components/Home/RestaurantMenu";
-import PostMenu from "../components/Home/PostMenu";
+import PostNoticeMini from "../components/Home/PostNoticeMini";
 import logo_text from "../assets/logo_text.png";
-//이거 넣는법 까먹음
+//이거 넣는법 까먹음 알아야됨
+
 // 음식 버튼 부분 component화 시켜서 개선
+//음식 버튼 색깔 이미지랑 다름
 
 const StringScreen = [
   { screen: "KoreanList" },
@@ -28,12 +30,40 @@ const StringScreen = [
 
 const Dummy = [
   {
-    title: "♚♚이더리움 비트☆코인♚♚가입시$$전원 0.01코인 지급☜☜100%증정",
-    like: 98766,
+    title: "테스트 게시글입니다1",
+    year: 2021,
+    month: 5,
+    day: 17,
   },
   {
-    title: "ddffddff",
-    like: 1,
+    title: "테스트 게시글입니다2",
+    year: 2021,
+    month: 10,
+    day: 17,
+  },
+  {
+    title: "테스트 게시글입니다3",
+    year: 2021,
+    month: 5,
+    day: 17,
+  },
+  {
+    title: "테스트 게시글입니다4",
+    year: 2021,
+    month: 5,
+    day: 17,
+  },
+  {
+    title: "테스트 게시글입니다5",
+    year: 2021,
+    month: 5,
+    day: 17,
+  },
+  {
+    title: "테스트 게시글입니다6",
+    year: 2021,
+    month: 5,
+    day: 17,
   },
 ];
 
@@ -41,7 +71,6 @@ const win = Dimensions.get("window");
 
 const Home = () => {
   const [text, onChangeText] = React.useState();
-
   return (
     <Screen>
       <Logo>
@@ -157,12 +186,57 @@ const Home = () => {
           </ButtonsSecondRow>
         </Buttons>
       </Foodlist>
-      <Post></Post>
+      <PostNotice>
+        <Post>
+          <Post_Title>
+            <Post_Title_Title>
+              <Post_Title_Title_Text>게시글</Post_Title_Title_Text>
+            </Post_Title_Title>
+            <Post_Title_Button>
+              <TouchableOpacity>
+                <Post_Title_Button_Text>더보기+</Post_Title_Button_Text>
+              </TouchableOpacity>
+            </Post_Title_Button>
+          </Post_Title>
+          <Post_Content>
+            <Post_Content_Content>
+              {Dummy.map((element, key) => (
+                <PostNoticeMini
+                  description={element.title}
+                  year={element.year}
+                  month={element.month}
+                  day={element.day}
+                  key={key}
+                />
+              ))}
+            </Post_Content_Content>
+          </Post_Content>
+        </Post>
+        <Notice>
+          <Notice_Title>
+            <Notice_Title_Title>
+              <Notice_Title_Title_Text>공지사항</Notice_Title_Title_Text>
+            </Notice_Title_Title>
+            <Notice_Title_Button>
+              <TouchableOpacity>
+                <Notice_Title_Button_Text>더보기+</Notice_Title_Button_Text>
+              </TouchableOpacity>
+            </Notice_Title_Button>
+          </Notice_Title>
+          <Notice_Content></Notice_Content>
+        </Notice>
+      </PostNotice>
+      <WhiteSpace />
     </Screen>
   );
 };
 
 export default Home;
+
+const WhiteSpace = styled.View`
+  width: 100%;
+  height: 5%;
+`;
 
 const Screen = styled.View`
   flex: 1;
@@ -187,7 +261,7 @@ const Logopos = styled.View`
 
 const Foodlist = styled.View`
   width: 100%;
-  height: 32%;
+  height: 28%;
   background-color: #eff0f4;
 `;
 
@@ -213,7 +287,7 @@ const Buttons = styled.View`
 `;
 
 const ButtonsFirstRow = styled.View`
-  width: 90%;
+  width: 95%;
   height: 40%;
   justify-content: center;
   align-items: center;
@@ -221,11 +295,12 @@ const ButtonsFirstRow = styled.View`
 `;
 
 const EachButton = styled.View`
-  width: 20%;
+  width: 17%;
+  margin: 0px;
 `;
 
 const ButtonsSecondRow = styled.View`
-  width: 90%;
+  width: 95%;
   height: 40%;
   justify-content: center;
   align-items: center;
@@ -233,9 +308,104 @@ const ButtonsSecondRow = styled.View`
   top: 3px;
 `;
 
+const PostNotice = styled.View`
+  width: 100%;
+  height: 53%;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Post = styled.View`
   width: 100%;
-  height: 52%;
+  height: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Post_Title = styled.View`
+  width: 90%;
+  height: 28%;
+  flex-direction: row;
+  bottom: 5px;
+`;
+
+const Post_Title_Title = styled.View`
+  width: 80%;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
+const Post_Title_Button = styled.View`
+  width: 20%;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const Post_Title_Title_Text = styled.Text`
+  font-size: 18px;
+`;
+
+const Post_Title_Button_Text = styled.Text`
+  font-size: 12px;
+`;
+
+const Post_Content = styled.View`
+  width: 90%;
+  height: 72%;
+  border: 0.5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Post_Content_Content = styled.View`
+  width: 100%;
+  height: 95%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Notice = styled.View`
+  width: 100%;
+  height: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Notice_Title = styled.View`
+  width: 90%;
+  height: 28%;
+  flex-direction: row;
+  bottom: 5px;
+`;
+
+const Notice_Title_Title = styled.View`
+  width: 80%;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
+const Notice_Title_Button = styled.View`
+  width: 20%;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const Notice_Title_Title_Text = styled.Text`
+  font-size: 18px;
+`;
+
+const Notice_Title_Button_Text = styled.Text`
+  font-size: 12px;
+`;
+
+const Notice_Content = styled.View`
+  width: 90%;
+  height: 72%;
+  border: 0.5px;
 `;
 
 const styles = StyleSheet.create({
