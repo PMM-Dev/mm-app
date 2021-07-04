5cdaf365a1eaba871a8b422278b531b7091c67e8
sjmjys954646 <sjmjys954646@gmail.com>
2021-07-05 00:57:38 +0900

    feat : map bookmark design

--- screen/Mapping.js
+++ screen/Mapping.js
@@ -22,6 +22,21 @@ import SearchbarMapPart from "../components/Map/SearchbarMapPart";
 
 const chartHeight = Dimensions.get("window").height;
 
+const dummy = [
+  {
+    title: "용봉동 길성유부",
+    address: "광주광역시 북구 용봉동 151-77번지 1층",
+    number: "062-266-1202",
+    rating: 4.5,
+  },
+  {
+    title: "용봉동 rlsdfkljsaklv",
+    address: "광주광역sdfasg77번지 1층",
+    number: "062-266adgjnsghf-1202",
+    rating: 5,
+  },
+];
+
 const Mapping = () => {
   const [marker, setmarker] = useState([
     {
@@ -52,6 +67,7 @@ const Mapping = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
   const [bookMarkPressed, setbookMarkPressed] = useState(false);
+  const [whichBookmark, setwhichBookmark] = useState(-1);
   const mapRef = React.createRef();
   const preLoad = async () => {
     let { status } = await Location.requestPermissionsAsync();
@@ -75,13 +91,7 @@ const Mapping = () => {
   return (
     <>
       {isLoading ? (
-        <View>
-          <ActivityIndicator
-            animating={true}
-            size="large"
-            color={Colors.red800}
-          />
-        </View>
+        <View></View>
       ) : (
         <View>
           <Scroll>
@@ -102,7 +112,10 @@ const Mapping = () => {
                   followUserLocation={true}
                   showsMyLocationButton={true}
                   ref={mapRef}
-                  onPress={() => setbookMarkPressed(false)}
+                  onPress={() => {
+                    setbookMarkPressed(false);
+                    setwhichBookmark(-1);
+                  }}
                 >
                   {marker.map((makrer, index) => (
                     <Marker
@@ -110,7 +123,10 @@ const Mapping = () => {
                       coordinate={makrer.latlng}
                       title={marker.title}
                       description={marker.description}
-                      onPress={() => setbookMarkPressed(true)}
+                      onPress={() => {
+                        setbookMarkPressed(true);
+                        setwhichBookmark(index);
+                      }}
                     >
                       <Callout tooltip={true}></Callout>
                     </Marker>
@@ -120,32 +136,68 @@ const Mapping = () => {
               </Marker> */}
                 </MapView>
                 <SearchbarMapPart />
-                {/* <PosButton
-              style={{
-                position: "absolute", //use absolute position to show button on top of the map
-                bottom: "5%", //for center align
-                right: "10%",
-                alignSelf: "flex-end", //for align to right
-              }}
-            >
-              <TouchableOpacity
-                mode="text"
-                color="#ffffff"
-                onPress={() => {
-                  mapRef.current.animateToRegion({
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                    latitudeDelta: 0.009,
-                    longitudeDelta: 0.009,
-                  });
-                }}
-                style={{ borderColor: "black" }}
-              >
-                <Image source={require("../assets/search_1.png")} />
-              </TouchableOpacity>
-            </PosButton> */}
+                {bookMarkPressed ? (
+                  <NotYet />
+                ) : (
+                  <PosButton
+                    style={{
+                      position: "absolute", //use absolute position to show button on top of the map
+                      bottom: "5%", //for center align
+                      right: "10%",
+                      alignSelf: "flex-end", //for align to right
+                    }}
+                  >
+                    <TouchableOpacity
+                      mode="text"
+                      color="#ffffff"
+                      onPress={() => {
+                        mapRef.current.animateToRegion({
+                          latitude: location.coords.latitude,
+                          longitude: location.coords.longitude,
+                          latitudeDelta: 0.009,
+                          longitudeDelta: 0.009,
+                        });
+                      }}
+                      style={{ borderColor: "black" }}
+                    >
+                      <Image source={require("../assets/search_1.png")} />
+                    </TouchableOpacity>
+                  </PosButton>
+                )}
               </Container>
-              {bookMarkPressed ? <Explanation></Explanation> : <NotYet />}
+              {bookMarkPressed ? (
+                <Explanation>
+                  <ExplanationUp>
+                    <ExplanationTitle>
+                      <ExplanationTitleText>
+                        {dummy[whichBookmark].title}
+                      </ExplanationTitleText>
+                    </ExplanationTitle>
+                    <ExplanationAddressContact>
+                      <ExplanationAddress>
+                        <ExplanationAddressText>
+                          주소: {dummy[whichBookmark].address}
+                        </ExplanationAddressText>
+                      </ExplanationAddress>
+                      <ExplanationContact>
+                        <ExplanationContactText>
+                          연락처: {dummy[whichBookmark].number}
+                        </ExplanationContactText>
+                      </ExplanationContact>
+                    </ExplanationAddressContact>
+                  </ExplanationUp>
+                  <ExplanationDown>
+                    <ExplanationImage>
+                      <ExplanationImageImg
+                        source={require("../assets/tmp.jpg")}
+                      />
+                    </ExplanationImage>
+                    <ExplanationRateButton></ExplanationRateButton>
+                  </ExplanationDown>
+                </Explanation>
+              ) : (
+                <NotYet />
+              )}
             </Wrapper>
           </Scroll>
         </View>
@@ -153,6 +205,71 @@ const Mapping = () => {
     </>
   );
 };
+
+const ExplanationImageImg = styled.Image`
+  height: 70%;
+  width: 80%;
+  border-radius: 15px;
+`;
+
+const ExplanationDown = styled.View`
+  height: 60%;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const ExplanationImage = styled.View`
+  height: 100%;
+  width: 35%;
+  border: 1px black;
+  align-items: center;
+`;
+
+const ExplanationRateButton = styled.View`
+  height: 100%;
+  width: 65%;
+`;
+
+const ExplanationUp = styled.View`
+  height: 40%;
+  width: 100%;
+  right: -4%;
+`;
+
+const ExplanationTitle = styled.View`
+  height: 50%;
+  width: 100%;
+  justify-content: center;
+`;
+
+const ExplanationTitleText = styled.Text`
+  font-size: 22px;
+`;
+
+const ExplanationAddressText = styled.Text`
+  font-size: 12px;
+  letter-spacing: 1px;
+`;
+
+const ExplanationContactText = styled.Text`
+  font-size: 12px;
+  letter-spacing: 1px;
+`;
+
+const ExplanationAddressContact = styled.View`
+  height: 50%;
+  width: 100%;
+`;
+
+const ExplanationAddress = styled.View`
+  height: 40%;
+  width: 100%;
+`;
+
+const ExplanationContact = styled.View`
+  height: 40%;
+  width: 100%;
+`;
 
 const NotYet = styled.View``;
 
@@ -171,6 +288,7 @@ const Explanation = styled.View`
   bottom: 0%;
   position: absolute;
   background-color: white;
+  border-radius: 10;
 `;
 
 const PosButton = styled.View`
--- screen/Post.js
+++ screen/Post.js
@@ -1,189 +1,194 @@
 import React from "react";
 import styled from "styled-components";
-import { Button} from "react-native-paper";
+import { Button } from "react-native-paper";
 import CommentText from "../components/CommentText";
 import { useState, useEffect } from "react";
-import {ScrollView, TextInput} from "react-native";
+import { ScrollView, TextInput } from "react-native";
 
-const Dummy = 
-  [{
-    writer : "YUNSU",
-    description: "창평국밥맛있나?" ,
-    like : 10,
+const Dummy = [
+  {
+    writer: "YUNSU",
+    description: "창평국밥맛있나?",
+    like: 10,
   },
   {
-    writer : "SOONCHAN",
-    description: "ㄴㄴ" ,
-    like : 5,
+    writer: "SOONCHAN",
+    description: "ㄴㄴ",
+    like: 5,
   },
   {
-    writer : "ASD",
-    description: "너 어디사냐?" ,
-    like : 3,
+    writer: "ASD",
+    description: "너 어디사냐?",
+    like: 3,
   },
   {
-    writer : "YUNSU",
-    description: "창평국밥맛있나?" ,
-    like : 10,
+    writer: "YUNSU",
+    description: "창평국밥맛있나?",
+    like: 10,
   },
   {
-    writer : "SOONCHAN",
-    description: "ㄴㄴ" ,
-    like : 5,
+    writer: "SOONCHAN",
+    description: "ㄴㄴ",
+    like: 5,
   },
   {
-    writer : "ASD",
-    description: "너 어디사냐?" ,
-    like : 3,
+    writer: "ASD",
+    description: "너 어디사냐?",
+    like: 3,
   },
 ];
 
-const Restaurant = ({route}) => {
-  
+const Restaurant = ({ route }) => {
   const [count, setCount] = useState([]);
-  useEffect(()=>{
-    setCount(Dummy)
-  },[]);
-  
-  const {title, like, writer, description, time} = route.params.data;
-  const [text, setText] = useState('');
+  useEffect(() => {
+    setCount(Dummy);
+  }, []);
+
+  const { title, like, writer, description, time } = route.params.data;
+  const [text, setText] = useState("");
   return (
     <Holder>
       <UpView>
-          <Title>
-            <RealTitle>
-                <TitleText>{title}</TitleText>
-            </RealTitle>
-            <TitleExcept>
-                <Writer>
-                    <TitleExceptFont>{writer}</TitleExceptFont>
-                </Writer>
-                <TimeWrite>
-                    <TitleExceptFont>{time.year}/{time.month}/{time.day}</TitleExceptFont>
-                    <TitleExceptFont>{time.hour}:{time.minute}</TitleExceptFont>
-                </TimeWrite>
-            </TitleExcept>
-          </Title>
-          <DescriptionView>
-              <DescriptionText>{description}</DescriptionText>
-          </DescriptionView>
-          <LikeView>
-                <Likenum>{like}</Likenum>
-                <Button
-                    icon = "thumb-up"
-                    color="#8A2BE2"
-                    onPress={() => console.log("Pressed")}
-                />           
-            </LikeView>
+        <Title>
+          <RealTitle>
+            <TitleText>{title}</TitleText>
+          </RealTitle>
+          <TitleExcept>
+            <Writer>
+              <TitleExceptFont>{writer}</TitleExceptFont>
+            </Writer>
+            <TimeWrite>
+              <TitleExceptFont>
+                {time.year}/{time.month}/{time.day}
+              </TitleExceptFont>
+              <TitleExceptFont>
+                {time.hour}:{time.minute}
+              </TitleExceptFont>
+            </TimeWrite>
+          </TitleExcept>
+        </Title>
+        <DescriptionView>
+          <DescriptionText>{description}</DescriptionText>
+        </DescriptionView>
+        <LikeView>
+          <Likenum>{like}</Likenum>
+          <Button
+            icon="thumb-up"
+            color="#8A2BE2"
+            onPress={() => console.log("Pressed")}
+          />
+        </LikeView>
       </UpView>
       <DownView>
         <ScrollView>
-            <CommentView>
-                {count.map((element,key) => 
-                    <CommentText key = {key} writer = {element.writer} description = {element.description} like = {element.like}/>
-                )}
-            </CommentView>
-          </ScrollView>
-        </DownView>
-        <Input>
-            <View>
-              <TextInput
-                placeholder="댓글을 입력하세요!"
-                onChangeText={text => setText(text)}
-                defaultValue={text}
-                style = {{flex : 1,
-                  height : 40,
-                }}
+          <CommentView>
+            {count.map((element, key) => (
+              <CommentText
+                key={key}
+                writer={element.writer}
+                description={element.description}
+                like={element.like}
               />
-            </View>
-            <Button
-              mode="contained"
-              color="#8A2BE2"
-              onPress={() => console.log("Pressed")}
-            >
-              <Text>글 작성!</Text>
-            </Button>
-        </Input>
+            ))}
+          </CommentView>
+        </ScrollView>
+      </DownView>
+      <Input>
+        <View>
+          <TextInput
+            placeholder="댓글을 입력하세요!"
+            onChangeText={(text) => setText(text)}
+            defaultValue={text}
+            style={{ flex: 1, height: 40 }}
+          />
+        </View>
+        <Button
+          mode="contained"
+          color="#8A2BE2"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text>글 작성!</Text>
+        </Button>
+      </Input>
     </Holder>
   );
 };
 
 const LikeView = styled.View`
-    height: 10%;
-    width:100%;
-    flexDirection: row;
-    align-items: center;
-    justify-content:flex-end;
+  height: 10%;
+  width: 100%;
+  flexdirection: row;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const Likenum = styled.Text``;
 
 const TitleText = styled.Text`
-    font-size : 18px;
+  font-size: 18px;
 `;
 
 const Title = styled.View`
-    height: 40%;
-    width:100%;
-    flexDirection: row;
+  height: 40%;
+  width: 100%;
+  flexdirection: row;
 `;
 
 const Writer = styled.View`
-    align-items: center;
-    justify-content:center;
-    height:50%;
-    `;
+  align-items: center;
+  justify-content: center;
+  height: 50%;
+`;
 
 const TimeWrite = styled.View`
-    align-items: center;
-    justify-content:center;
-    height:50%;
+  align-items: center;
+  justify-content: center;
+  height: 50%;
 `;
 
-const TitleExceptFont = styled.Text`
-`;
+const TitleExceptFont = styled.Text``;
 
 const RealTitle = styled.View`
-    height: 100%;
-    width:70%;
-    align-items: center;
-    justify-content:center;
+  height: 100%;
+  width: 70%;
+  align-items: center;
+  justify-content: center;
 `;
 
-const TitleExcept = styled.View`    
-    height: 100%;
-    width:30%;
-    flexDirection: column;
+const TitleExcept = styled.View`
+  height: 100%;
+  width: 30%;
+  flexdirection: column;
 `;
 
 const DescriptionView = styled.View`
-    height: 50%;
-    width:100%;
-    align-items: center;
-    justify-content:center;
+  height: 50%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 `;
 
-const Text = styled.Text`
-`;
+const Text = styled.Text``;
 
 const DescriptionText = styled.Text`
-    text-align: center;
+  text-align: center;
 `;
 
 const Input = styled.View`
-  height:20%;
-  width:100%;`;
-
-const UpView = styled.View` 
-  height:40%;
-  width:100%;
-  align-items: center;
-  justify-content:center;
+  height: 20%;
+  width: 100%;
 `;
 
-const DownView = styled.View` 
-  height:40%;
-  width:100%; 
+const UpView = styled.View`
+  height: 40%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DownView = styled.View`
+  height: 40%;
+  width: 100%;
 `;
 
 const Holder = styled.View`
@@ -195,15 +200,13 @@ const Holder = styled.View`
 
 const CommentView = styled.View`
   align-items: center;
-  height:70%;
-  width:100%; 
+  height: 70%;
+  width: 100%;
 `;
 
 const View = styled.View`
-  flex:1;
-  border : 1px black;
+  flex: 1;
+  border: 1px black;
 `;
-
-
 
 export default Restaurant;
