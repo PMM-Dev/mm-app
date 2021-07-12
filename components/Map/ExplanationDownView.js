99ac050997f01c77a828ac70d1d7f71e8c3240ee
sjmjys954646 <sjmjys954646@gmail.com>
2021-07-12 10:40:58 +0900

    fixing

---
+++ components/Map/ExplanationDownView.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Image, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import StarMaker from "./StarMaker";
+
+const ExplanationDownView = ({ data }) => {
+  return (
+    <ExplanationDown>
+      <ExplanationImage>
+        <ExplanationImageImg source={require("../../assets/tmp.jpg")} />
+      </ExplanationImage>
+      <ExplanationRateButton>
+        <ExplanationRate>
+          <StarMaker rate={data.rating} />
+        </ExplanationRate>
+        <ExplanationTag></ExplanationTag>
+        <ExplanationMoreButton>
+          <TouchableOpacity>
+            <ExplanationMoreButtonText>
+              후기 더 보러가기 +
+            </ExplanationMoreButtonText>
+          </TouchableOpacity>
+        </ExplanationMoreButton>
+      </ExplanationRateButton>
+    </ExplanationDown>
+  );
+};
+
+const ExplanationMoreButtonText = styled.Text``;
+
+const ExplanationMoreButton = styled.View`
+  height: 30%;
+  width: 100%;
+`;
+
+const ExplanationTag = styled.View`
+  height: 25%;
+  width: 100%;
+`;
+
+const ExplanationRate = styled.View`
+  height: 45%;
+  width: 100%;
+`;
+
+const ExplanationImageImg = styled.Image`
+  height: 90%;
+  width: 80%;
+  border-radius: 15px;
+`;
+
+const ExplanationDown = styled.View`
+  height: 60%;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const ExplanationImage = styled.View`
+  height: 100%;
+  width: 35%;
+  align-items: center;
+`;
+
+const ExplanationRateButton = styled.View`
+  height: 100%;
+  width: 65%;
+`;
+
+export default ExplanationDownView;
---
+++ components/Map/ExplanationUpView.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Image, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+
+const ExplanationUpView = ({ data }) => {
+  console.log(data);
+  return (
+    <ExplanationUp>
+      <ExplanationTitle>
+        <ExplanationTitleText>{data.title}</ExplanationTitleText>
+      </ExplanationTitle>
+      <ExplanationAddressContact>
+        <ExplanationAddress>
+          <ExplanationAddressText>주소: {data.address}</ExplanationAddressText>
+        </ExplanationAddress>
+        <ExplanationContact>
+          <ExplanationContactText>연락처: {data.number}</ExplanationContactText>
+        </ExplanationContact>
+      </ExplanationAddressContact>
+      <HeartButtonPos>
+        <TouchableOpacity>
+          {data.bookmarked ? (
+            <HeartImg source={require("../../assets/heart_2.png")} />
+          ) : (
+            <HeartImg source={require("../../assets/heart_1.png")} />
+          )}
+        </TouchableOpacity>
+      </HeartButtonPos>
+    </ExplanationUp>
+  );
+};
+
+const HeartImg = styled.Image`
+  height: 100%;
+  width: 100%;
+`;
+
+const HeartButtonPos = styled.View`
+  position: absolute;
+  width: 10%;
+  height: 40%;
+  top: 15%;
+  right: 8%;
+`;
+
+const ExplanationUp = styled.View`
+  height: 40%;
+  width: 100%;
+  right: -4%;
+`;
+
+const ExplanationTitle = styled.View`
+  height: 50%;
+  width: 100%;
+  justify-content: center;
+`;
+
+const ExplanationTitleText = styled.Text`
+  font-size: 22px;
+`;
+
+const ExplanationAddressText = styled.Text`
+  font-size: 12px;
+  letter-spacing: 1px;
+`;
+
+const ExplanationContactText = styled.Text`
+  font-size: 12px;
+  letter-spacing: 1px;
+`;
+
+const ExplanationAddressContact = styled.View`
+  height: 50%;
+  width: 100%;
+`;
+
+const ExplanationAddress = styled.View`
+  height: 40%;
+  width: 100%;
+`;
+
+const ExplanationContact = styled.View`
+  height: 40%;
+  width: 100%;
+`;
+
+export default ExplanationUpView;
