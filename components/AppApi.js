90f197bfd1a9b5995b63719ce12532d2213efd1a
Kwon770 <sckwon770@gmail.com>
2021-08-02 19:21:20 +0900

    strange

--- components/AppApi.js
+++ components/AppApi.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { API_URL, API_TOKEN } from "@env";
+import {API_URL} from '@env';
 
 export const getRestaurantList = async (genre) => {
-  try {
-    const response = await axios.get(
-      API_URL + "/api/restaurant/condition" + "?type=" + genre
-    );
-    return response.data;
-  } catch (e) {
-    console.log("[AppApi][Exception] " + e);
-    return [];
-  }
+    try {
+        const response = await axios.get(
+            API_URL + "/api/restaurant/condition" + "?type=" + genre
+        );
+        return response.data;
+    } catch (e) {
+        console.log("[AppApi][Exception] " + e);
+        return [];
+    }
 };
 
