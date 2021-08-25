1345acab9c71cd3275be8044bd2dc5ddce1a638b
Kwon770 <sckwon770@gmail.com>
2021-08-25 22:15:06 +0900

    feat: Append authorization header on AppApi

--- components/Api/AppApi.js
+++ components/Api/AppApi.js
@@ -2,22 +2,16 @@ import axios from "axios";
 import {API_URL} from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const getAuthHeader = async () => {
-    const token = await AsyncStorage.getItem("@jwtToken");
-    console.log(token);
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-};
-
 export const getRestaurantsByGenre = async (genre) => {
     try {
-        const authHeaderConfig = getAuthHeader();
+        const token = await AsyncStorage.getItem("@jwtToken");
         const response = await axios.get(
             API_URL + "/api/restaurant/condition" + "?type=" + genre,
-            authHeaderConfig
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
         );
         return response.data;
     } catch (e) {
@@ -28,10 +22,14 @@ export const getRestaurantsByGenre = async (genre) => {
 
 export const getRestaurantComments = async (id) => {
     try {
-        const authHeaderConfig = getAuthHeader();
+        const token = await AsyncStorage.getItem("@jwtToken");
         const response = await axios.get(
             API_URL + "/api/restaurant/" + id + "/review",
-            authHeaderConfig
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
         );
         return response.data;
     } catch (e) {
@@ -42,17 +40,21 @@ export const getRestaurantComments = async (id) => {
 
 export const postRestaurantComment = async (review, userEmail, id) => {
     try {
-        const authHeaderConfig = getAuthHeader();
+        const token = await AsyncStorage.getItem("@jwtToken");
         const response = await axios.post(
             API_URL + "/api/restaurant/" + id + "/review",
-            authHeaderConfig,
             {
                 authorEmail: userEmail,
                 description: review,
                 grade: 4,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
             }
         );
-        return response;
+        return response.data;
     } catch (e) {
         console.error("[AppApi][Exception] " + e);
         return [];
