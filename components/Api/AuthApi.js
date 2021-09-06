676d8031d67bd64fd1dcd1d678dd1ac269bdc555
sjmjys954646 <sjmjys954646@gmail.com>
2021-09-06 22:10:10 +0900

    modify

--- components/Api/AuthApi.js
+++ components/Api/AuthApi.js
@@ -1,47 +1,50 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import {API_URL} from "@env";
+import { API_URL } from "@env";
 
 export const register = async (memberRequestDto) => {
-    try {
-        const response = await axios.post(API_URL + "/auth/register", {...memberRequestDto});
-        return response.data;
-    } catch (e) {
-        console.error("[AuthApi][Exception] failed register() " + e);
-        return undefined;
-    }
+  try {
+    const response = await axios.post(API_URL + "/auth/register", {
+      ...memberRequestDto,
+    });
+    return response.data;
+  } catch (e) {
+    console.error("[AuthApi][Exception] failed register() " + e);
+    return undefined;
+  }
 };
 
 export const getJwtTokenBySocialToken = async (memberRequestDto) => {
-    try {
-        const response = await axios.post(API_URL + "/auth/login",
-            {
-                ...memberRequestDto
-            })
-        return response.data;
-    } catch (e) {
-        console.log("[AuthApi][Exception] failed getJwtTokenBySocialToken() " + e);
-        return undefined;
-    }
+  try {
+    const response = await axios.post(API_URL + "/auth/login", {
+      ...memberRequestDto,
+    });
+    return response.data;
+  } catch (e) {
+    console.log("[AuthApi][Exception] failed getJwtTokenBySocialToken() " + e);
+    return undefined;
+  }
 };
 
 export const reissueJwtAccessToken = async () => {
-    try {
-        const jwtAccessToken = await AsyncStorage.getItem("@jwtAccessToken");
-        const jwtRefreshToken = await AsyncStorage.getItem("@jwtRefreshToken");
+  try {
+    const jwtAccessToken = await AsyncStorage.getItem("@jwtAccessToken");
+    const jwtRefreshToken = await AsyncStorage.getItem("@jwtRefreshToken");
 
-        const response = await axios.post(API_URL + "/auth/reissue",
-            {
-                accessToken: jwtAccessToken,
-                refreshToken: jwtRefreshToken
-            });
-        await AsyncStorage.setItem("@jwtAccessToken", response.accessToken);
-        await AsyncStorage.setItem("@jwtRefreshToken", response.refreshToken)
-        await AsyncStorage.setItem("@jwtTokenExpiresIn", JSON.stringify(response.accessTokenExpiresIn));
+    const response = await axios.post(API_URL + "/auth/reissue", {
+      accessToken: jwtAccessToken,
+      refreshToken: jwtRefreshToken,
+    });
+    await AsyncStorage.setItem("@jwtAccessToken", response.accessToken);
+    await AsyncStorage.setItem("@jwtRefreshToken", response.refreshToken);
+    await AsyncStorage.setItem(
+      "@jwtTokenExpiresIn",
+      JSON.stringify(response.accessTokenExpiresIn)
+    );
 
-        return response.accessToken;
-    } catch (e) {
-        console.log("[AuthApi][Exception] failed reissueJwtAccessToken() " + e);
-        return undefined;
-    }
-}
+    return response.accessToken;
+  } catch (e) {
+    console.log("[AuthApi][Exception] failed reissueJwtAccessToken() " + e);
+    return undefined;
+  }
+};
