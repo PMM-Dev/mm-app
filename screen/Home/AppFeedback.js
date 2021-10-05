519d5c6c4d47b9006b2b369c7d572e93c6cfe545
sjmjys954646 <sjmjys954646@gmail.com>
2021-10-05 13:52:21 +0900

    feat : appfeedback page

--- screen/Home/AppFeedback.js
+++ screen/Home/AppFeedback.js
@@ -1,13 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import constants from "../../constants";
-import { ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator, TextInput } from "react-native-paper";
 import Theme from "../../style/Theme";
 import Feedback from "../../components/Home/Restaurant/Feedback";
 import Header from "../../components/Header/Header";
+import RBSheet from "react-native-raw-bottom-sheet";
 
 const AppFeedback = ({ route, navigation }) => {
   const [feedbacks, setFeedbacks] = useState();
+  const feedbackWritingPanelRef = useRef();
+  const [writingReviewContent, setWritingReviewContent] = useState("");
+
+  const requestPostingFeedback = async () => {
+    // const response = await postReview(
+    //   writingReviewContent,
+    //   writingReviewGrade,
+    //   restaurantId
+    // );
+    if (response === undefined) {
+      alert("리뷰 작성에 실패했습니다.");
+    }
+
+    closeReviewWritingPanel();
+  };
 
   const headerTitle = "피드백 목록";
 
@@ -32,6 +48,14 @@ const AppFeedback = ({ route, navigation }) => {
     },
   ];
 
+  const openReviewWritingPanel = () => {
+    feedbackWritingPanelRef.current.open();
+  };
+
+  const closeReviewWritingPanel = () => {
+    feedbackWritingPanelRef.current.close();
+  };
+
   useEffect(() => {
     async function requestMeFeedback() {}
     requestMeFeedback();
@@ -39,6 +63,56 @@ const AppFeedback = ({ route, navigation }) => {
   return (
     <Page>
       <Header route={route} navigation={navigation} title={headerTitle} />
+
+      <Button onPress={() => openReviewWritingPanel()}>
+        <ReviewWritingButtonText>피드백 작성하기</ReviewWritingButtonText>
+      </Button>
+      <RBSheet
+        ref={feedbackWritingPanelRef}
+        height={constants.vh(93)}
+        customStyles={{ container: { borderRadius: constants.vw(3) } }}
+      >
+        <ReviewWritingPanel>
+          <TopMenusHolder>
+            <Button onPress={closeReviewWritingPanel}>
+              <ButtonText>취소</ButtonText>
+            </Button>
+            <PanelTitle>피드백 작성하기</PanelTitle>
+            <Button onPress={requestPostingFeedback}>
+              <ButtonText>보내기</ButtonText>
+            </Button>
+          </TopMenusHolder>
+          <TextInput
+            mode="none"
+            value={writingReviewContent}
+            selectionColor={Theme.fontBlue}
+            outlineColor={Theme.fontBlue}
+            multiline={true}
+            onChangeText={(text) => setWritingReviewContent(text)}
+            style={{
+              width: "100%",
+              height: constants.vh(5),
+              backgroundColor: Theme.backgroundWhite,
+              alignItems: "flex-start",
+            }}
+            right={<TextInput.Affix tex={"/100"} />}
+          />
+          {/*<PicturesView>*/}
+          {/*    <UploadPictureButton onPress={pickImage}>*/}
+          {/*        <UploadPictureIcon*/}
+          {/*            source={RESTAURANT_ICON_IMAGE}*/}
+          {/*            style={{ tintColor: Theme.fontBlack }}*/}
+          {/*        />*/}
+          {/*    </UploadPictureButton>*/}
+          {/*    <UploadedPictureHolder>*/}
+          {/*        <UploadedPicture source={{ uri: firstImage }} />*/}
+          {/*    </UploadedPictureHolder>*/}
+          {/*    <UploadedPictureHolder>*/}
+          {/*        <UploadedPicture source={{ uri: secondImage }} />*/}
+          {/*    </UploadedPictureHolder>*/}
+          {/*</PicturesView>*/}
+        </ReviewWritingPanel>
+      </RBSheet>
       <Scroll>
         {dummy ? (
           dummy.map((feedback, index) => {
@@ -51,6 +125,41 @@ const AppFeedback = ({ route, navigation }) => {
     </Page>
   );
 };
+
+const ReviewWritingButtonText = styled.Text`
+  margin-left: ${constants.vw(3)}px;
+  ${(props) => props.theme.NanumSquareBFont}
+  font-size: ${constants.vw(4)}px;
+  color: ${(props) => props.theme.fontBlue};
+`;
+
+const Button = styled.TouchableOpacity``;
+
+const ButtonText = styled.Text`
+  ${(props) => props.theme.NanumSquareRFont}
+  font-size: ${constants.vw(4)}px;
+  color: ${(props) => props.theme.fontBlue};
+`;
+
+const PanelTitle = styled.Text`
+  ${(props) => props.theme.NanumSquareBFont}
+  font-size: ${constants.vw(4.5)}px;
+`;
+
+const TopMenusHolder = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 7%;
+`;
+
+const ReviewWritingPanel = styled.View`
+  width: 100%;
+  height: 100%;
+  padding: 5% 5%;
+  align-items: center;
+`;
 
 const Page = styled.View`
   width: 100%;
