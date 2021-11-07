fbc8f6d0eca542158db890bfa8c2bb940c46cd04
sjmjys954646 <sjmjys954646@gmail.com>
2021-11-07 23:15:58 +0900

    feat : PostPart

--- components/Home/PostPart.js
+++ components/Home/PostPart.js
@@ -5,66 +5,59 @@ import constants from "../../constants";
 import NotPreparedAnnouncement from "../NotPreparedAnnouncement";
 
 const Dummy = [
-  {
-    title: "테스트 게시글입니다1",
-    year: 2021,
-    month: 5,
-    day: 17,
-  },
-  {
-    title: "테스트 게시글입니다2",
-    year: 2021,
-    month: 10,
-    day: 17,
-  },
-  {
-    title: "테스트 게시글입니다3",
-    year: 2021,
-    month: 5,
-    day: 17,
-  },
-  {
-    title: "테스트 게시글입니다4",
-    year: 2021,
-    month: 5,
-    day: 17,
-  },
-  {
-    title: "테스트 게시글입니다5",
-    year: 2021,
-    month: 5,
-    day: 17,
-  },
-  {
-    title: "테스트 게시글입니다6",
-    year: 2021,
-    month: 5,
-    day: 17,
-  },
+  {Title : "[전대 후문]김해뒷고기 후기", ID : "asdf", visitNum: 30, recommendNum : 2, date : "10.26"},
+  {Title : "김해뒷고기 후기", ID : "asdf", visitNum: 30, recommendNum : 2, date : "10.26"},
+  {Title : "김해뒷고기 후기", ID : "asdf", visitNum: 30, recommendNum : 2, date : "10.26"},
 ];
 
 const PostPart = () => {
   return (
     <HomePart>
-      <NotPreparedAnnouncement />
       <Header>
           <Title>자유게시판</Title>
-          <MoreButton>더보기 +</MoreButton>
+          <MoreButton>
+            <MoreButtonText>더보기 +</MoreButtonText>
+          </MoreButton>
       </Header>
       <Content>
           {Dummy.map((element, key) => (
-            <PostNoticeMini
-              description={element.title}
-              year={element.year}
-              month={element.month}
-              day={element.day}
-              key={key}
-            />
+            <Card key = {key}>
+              <CardTitle>{element.Title}</CardTitle>
+              <CardExplanation>
+                <CardExplanationText>{element.ID} | 조회수 : {element.visitNum} | 추천 : {element.recommendNum}</CardExplanationText>
+                <CardExplanationDate>{element.date}</CardExplanationDate>
+              </CardExplanation>
+            </Card>
           ))}
       </Content>
     </HomePart>
   );
 };
+
+const CardExplanationDate = styled.Text`
+  ${(props) => props.theme.NanumGothicBoldFont};
+  font-size: ${constants.vw(2.5)}px;
+`;
+
+const CardExplanationText = styled.Text`
+  ${(props) => props.theme.NanumGothicBoldFont};
+  font-size: ${constants.vw(2.5)}px;
+  width : 90%;
+`;
+
+const CardExplanation = styled.View`
+  flex-direction: row;
+`;
+
+const CardTitle = styled.Text`
+  ${(props) => props.theme.NanumGothicBoldFont};
+  font-size: ${constants.vw(4)}px;
+  margin-bottom: ${constants.vh(1)}px;
+`;
+
+const Card = styled.View`
+  height : 33%;
+`
 
 const HomePart = styled.View`
   width: 100%;
@@ -73,7 +66,6 @@ const HomePart = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
   background-color: ${(props) => props.theme.backgroundWhite};
   border-bottom-width: 1px;
   border-bottom-color: ${(props) => props.theme.fontGray};
@@ -86,7 +78,7 @@ const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: flex-end;
-  margin-bottom: 2.5%;
+  margin-bottom: 4%;
 `;
 
 const Title = styled.Text`
@@ -94,7 +86,9 @@ ${(props) => props.theme.NanumGothicBoldFont};
 font-size: ${constants.vw(5)}px;;
 `;
 
-const MoreButton = styled.Text`
+const MoreButton = styled.TouchableOpacity``;
+
+const MoreButtonText = styled.Text`
   ${(props) => props.theme.NanumSquareRFont}
   font-size: ${constants.vw(3.3)}px;
   color: ${(props) => props.theme.hlOrange};
@@ -103,8 +97,8 @@ const MoreButton = styled.Text`
 const Content = styled.View`
   width: 90%;
   height: 72%;
-  border: 0.5px;
   border-radius: ${constants.vw(1)}px;
+  padding-bottom: 2%;
 `;
 
 export default PostPart;
